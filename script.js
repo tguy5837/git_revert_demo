4c82715d9c55dd82c66d2afa31b9c5ac99292d90
DTGuy <tguy5837@gmail.com>
2022-08-22 20:16:05 -0400

    added autocomplete widget

--- script.js
+++ script.js
@@ -29,3 +29,30 @@ var handleFormSubmit = function (event) {
 };
 
 formEl.on('submit', handleFormSubmit);
+
+// Autocomplete widget
+$(function () {
+  var skillNames = [
+    'Bootstrap',
+    'C',
+    'C++',
+    'CSS',
+    'Express.js',
+    'Git',
+    'HTML',
+    'Java',
+    'JavaScript',
+    'jQuery',
+    'JSON',
+    'MySQL',
+    'Node.js',
+    'NoSQL',
+    'PHP',
+    'Python',
+    'React',
+    'Ruby',
+  ];
+  $('#skill-name').autocomplete({
+    source: skillNames,
+  });
+});
